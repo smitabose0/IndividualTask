9f7bb98d62284350d541591850baef0a256f18dc
jivadixit1@gmail.com <jivadixit1@gmail.com>
2023-05-08 14:02:06 +0530

    change after clone

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+m
+=Java/src/test/java/com/github/vitalliuss/helloci/AppTest.java,5/3/536266755dcc57e49059f1e7daeefd22f883594b
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
---
+++ Java/src/main/java/com/helloci/App.java
@@ -0,0 +1,17 @@
+package com.helloci;
+
+/**
+ * Hello CI!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello CI!" );
+    }
+    
+    public static void doNothing() {
+    	// do nothing
+    }
+}
